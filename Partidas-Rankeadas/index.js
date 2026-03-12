5f6efe9d5165f5940c2d40a6909d637dc7940a42
Amanda <amandareis401@gmail.com>
2026-03-12 19:29:26 -0300

    refactor: Corrige a lógica de nível e refatora o código

--- Partidas-Rankeadas/index.js
+++ Partidas-Rankeadas/index.js
@@ -2,25 +2,35 @@ function saldoRankeadas(quantVitorias, quantderrotas) {
   return quantVitorias - quantderrotas;
 }
 
-let nivelExp = saldoRankeadas(100, 40);
-let nivel = "";
-
-if (nivelExp < 10) {
-  nivel = "Ferro";
-} else if (nivelExp > 11 && nivelExp < 20) {
-  nivel = "Bronze";
-} else if (nivelExp > 21 && nivelExp < 50) {
-  nivel = "Prata";
-} else if (nivelExp > 51 && nivelExp < 80) {
-  nivel = "Ouro";
-} else if (nivelExp > 81 && nivelExp < 90) {
-  nivel = "Diamante";
-} else if (nivelExp > 91 && nivelExp < 100) {
-  nivel = "Lendário";
-} else if (nivelExp >= 100) {
-  nivel = "Imortal";
+function getNivel(saldo) {
+  let nivel;
+  if (saldo <= 10) {
+    nivel = "Ferro";
+  } else if (saldo <= 20) {
+    nivel = "Bronze";
+  } else if (saldo <= 50) {
+    nivel = "Prata";
+  } else if (saldo <= 80) {
+    nivel = "Ouro";
+  } else if (saldo <= 90) {
+    nivel = "Diamante";
+  } else if (saldo <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+  return nivel;
 }
 
-console.log("O héroi tem saldo de: " + nivelExp + " e está no nível: " + nivel); 
+const vitorias = 100;
+const derrotas = 40;
 
+let saldoVitorias = saldoRankeadas(vitorias, derrotas);
+let nivelHeroi = getNivel(saldoVitorias);
 
+console.log(
+  "O Herói tem um saldo de " +
+    saldoVitorias +
+    " e está no nível " +
+    nivelHeroi
+);
